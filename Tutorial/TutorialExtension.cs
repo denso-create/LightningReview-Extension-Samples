6f28a9423bca763dc438db6ce2bd83d5da6dd8c8
yus-sasaki <yus-sasaki@users.noreply.github.com>
2022-09-27 13:39:50 +0900

    チュートリアルエクステンションを格納

---
+++ Tutorial/TutorialExtension.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Windows;
+using DensoCreate.TutorialExtension.Commands;
+using LightningReview.ExtensionFramework;
+
+namespace DensoCreate.TutorialExtension
+{
+	/// <summary>
+	/// チュートリアルエクステンション
+	/// </summary>
+	[ExtensionExport("DensoCreate.Tutorial", "チュートリアルエクステンション","Tutorial Extension")]
+	public class TutorialExtension : Extension
+	{
+		#region オーバーライド
+
+		/// <summary>
+		/// アクティベート処理
+		/// </summary>
+		protected override void OnActivate()
+		{
+			// メインメニューの [ツール] - [プロパティの表示順を変更] の1つ上(Before)にメニューを作成する例
+			RegisterMenu()
+				.Location("ReviewWindow/MainMenu")
+				.Parent("ツール")
+				.Before("プロパティの表示順を変更")
+				.Command<AddIssuesCommand>()
+				.Build();
+
+			// メインメニューの [ツール] - [プロパティの表示順を変更] の1つ上(Before)にメニューを作成する例
+			// WithSeparatorAfter()でメニューの下にセパレーターを置くことができる
+			RegisterMenu()
+				.Location("ReviewWindow/MainMenu")
+				.Parent("ツール")
+				.Before("プロパティの表示順を変更")
+				.Command<MessageSettingCommand>()
+				.WithSeparatorAfter()
+				.Build();
+
+			// ツールバーのピポット分析ボタンの後(After)にボタンを作成する例
+			// SetButtonでボタンを配置できる。SetTextでテキスト形式のボタンを配置できる。
+			RegisterToolBarItem()
+					.Location("ReviewWindow/ToolBar")
+					.After("ピボット分析")
+					.SetButton()
+					.SetTextStyle()
+					.Command<MessageSettingCommand>()
+					.Build();
+
+			// ツールバーのピポット分析ボタンの後(After)に、メニューを作成する例
+			// SetToggleButtonでトグルボタンを配置できる。SetImageStyleでイメージで表現するボタンを配置できる。
+			RegisterToolBarItem()
+				.Location("ReviewWindow/ToolBar")
+				.After("ピボット分析")
+				.WithSeparatorBefore()
+				.SetButton()
+				.SetImageStyle()
+				.Command<AddIssuesCommand>()
+				.Build();
+			
+			// 保存後イベントを使う例
+			// 保存が完了すると、メッセージボックスが出る
+			App.ReviewAfterSave += AppOnReviewAfterSave;
+		}
+
+		#endregion
+
+		#region イベントハンドラ
+
+		/// <summary>
+		/// レビューファイル保存後にメッセージボックスを出すイベントハンドラ
+		/// </summary>
+		/// <param name="arg1"></param>
+		/// <param name="arg2"></param>
+		private void AppOnReviewAfterSave(object arg1, ReviewEventArgs arg2)
+		{
+			// ドキュメントの数と指摘の数を取得
+			var documentCount = arg2.ReviewWindow.Review.Documents.Count();
+			var issueCount = arg2.ReviewWindow.Review.GetAllIssues().Count();
+
+			var saveMassage = $"ドキュメントが{documentCount}件、 指摘は{issueCount}件です！";
+			MessageBox.Show(saveMassage, "Lightning Review", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		#endregion
+	}
+}
---
+++ Tutorial/Views/MessageSettingDialog.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using LightningReview.ExtensionFramework;
+
+namespace DensoCreate.TutorialExtension.Views
+{
+	/// <summary>
+	/// MessageSettingDialog.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MessageSettingDialog : Window
+	{
+		#region 構築
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public MessageSettingDialog()
+		{
+			InitializeComponent();
+
+			// ダイアログの背景色と文字色をテーマ色に合わせる
+			this.SetThemeColor();
+		}
+
+		#endregion
+
+		#region プロパティ
+
+		/// <summary>
+		/// テキストボックスに設定するテキスト
+		/// </summary>
+		public string Text
+		{
+			get => m_TextBox.Text;
+			set => m_TextBox.Text = value;
+		}
+
+		/// <summary>
+		/// 選択された日付
+		/// </summary>
+		public DateTime? SelectedDateTime
+		{
+			get => m_DateTimePicker.SelectedDateTime;
+			set => m_DateTimePicker.SelectedDateTime = value;
+		}
+
+		#endregion
+
+
+		#region 内部サービス
+
+		/// <summary>
+		/// OKボタンクリック時のイベントハンドラ
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void M_OKButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = true;
+		}
+
+		#endregion
+	}
+}
